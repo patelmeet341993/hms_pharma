a27ccc4c3d97c391c84222ead9c001208c3bb166
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2022-09-05 15:25:25 +0530

    Login backend (#1)
    * added admin_user_provider.dart to store logged in user data
    
    * added authentication_controller.dart to perform login operations
    
    * added admin_user_provider in MultiProvider
    
    * added fluttertoast package to show toast in app
    
    * added MyToast class to show toast in app
    
    * added loggedInUser key in SharePrefrenceKeys to store loggedin user data
    
    * added admin_user_controller.dart for Admin User creation and other user operations
    
    * added equatable package
    
    * 1) added description, imageUrl and scannerData keys in AdminUserModel
    2) props in AdminUserModel to compare to objects on AdminUserModel
    
    * removed RandomBool Generator for page navigation and called isUserLoggedIn to navigate to login or home screen in splash screen
    
    * added common LoadingWidget to show loading
    
    * added ModalProgressHUD to show loading in page
    
    * added a basic form to login using username and password in LoginScreen
    
    * added logout button and showing logged in user name and role in HomeScreen
    
    * added AppStrings class to store strings for Ui and Toast messages etc.
    
    * Changed title of Web App

--- lib/views/homescreen/homescreen.dart
+++ lib/views/homescreen/homescreen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../controllers/authentication_controller.dart';
+import '../../models/admin_user_model.dart';
+import '../../providers/admin_user_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/HomeScreen";
@@ -15,9 +20,46 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text("Home Screen"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                AuthenticationController().logout(context: context);
+              },
+              icon: Icon(Icons.logout),
+            )
+          ],
         ),
         body: Center(
-          child: Text("Home Body"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Home Body"),
+              SizedBox(height: 20,),
+              Consumer<AdminUserProvider>(
+                builder: (BuildContext context, AdminUserProvider adminUserProvider, Widget? child) {
+                  AdminUserModel? adminUserModel = adminUserProvider.getAdminUserModel();
+                  if(adminUserModel == null) {
+                    return Text("Not Logged in");
+                  }
+                  return Column(
+                    children: [
+                      Text("User Name:${adminUserProvider.getAdminUserModel()!.name}"),
+                      Text("User Role:${adminUserProvider.getAdminUserModel()!.role}"),
+                    ],
+                  );
+                },
+              ),
+              SizedBox(height: 20,),
+              FlatButton(
+                onPressed: () {
+                  // VisitController().createDummyVisitDataInFirestore();
+                  // PatientController().createDummyPatientDataInFirestore();
+                },
+                child: Text("Create Visit"),
+              ),
+            ],
+          ),
         ),
       ),
     );
