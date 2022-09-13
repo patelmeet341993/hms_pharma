5278da08cf9aa2067923f3adc9249396216fe974
Dishant <agrawaldishant29@gmail.com>
2022-09-13 12:40:38 +0530

    added hospitalId, isUndeletable and createdTime in AdminUserModel

--- lib/models/admin_user_model.dart
+++ lib/models/admin_user_model.dart
@@ -1,11 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 import '../utils/parsing_helper.dart';
 
 class AdminUserModel extends Equatable {
-  String id = "", name = "", username = "", password = "", role = "", description = "", imageUrl = "";
+  String id = "", name = "", username = "", password = "", role = "", description = "", imageUrl = "", hospitalId = "";
   Map<String, dynamic> scannerData = <String, dynamic>{};
-  bool isActive = false;
+  bool isActive = false, isUndeletable = false;
+  Timestamp? createdTime;
 
   AdminUserModel({
     this.id = "",
@@ -15,8 +17,11 @@ class AdminUserModel extends Equatable {
     this.role = "",
     this.description = "",
     this.imageUrl = "",
+    this.hospitalId = "",
     this.scannerData = const <String, dynamic>{},
     this.isActive = false,
+    this.isUndeletable = false,
+    this.createdTime,
   });
 
   AdminUserModel.fromMap(Map<String, dynamic> map) {
@@ -27,8 +32,11 @@ class AdminUserModel extends Equatable {
     role = ParsingHelper.parseStringMethod(map['role']);
     description = ParsingHelper.parseStringMethod(map['description']);
     imageUrl = ParsingHelper.parseStringMethod(map['imageUrl']);
+    hospitalId = ParsingHelper.parseStringMethod(map['hospitalId']);
     scannerData = ParsingHelper.parseMapMethod<dynamic, dynamic, String, dynamic>(map['scannerData']);
     isActive = ParsingHelper.parseBoolMethod(map['isActive']);
+    isUndeletable = ParsingHelper.parseBoolMethod(map['isUndeletable']);
+    createdTime = ParsingHelper.parseTimestampMethod(map['createdTime']);
   }
 
   void updateFromMap(Map<String, dynamic> map) {
@@ -39,11 +47,14 @@ class AdminUserModel extends Equatable {
     role = ParsingHelper.parseStringMethod(map['role']);
     description = ParsingHelper.parseStringMethod(map['description']);
     imageUrl = ParsingHelper.parseStringMethod(map['imageUrl']);
+    hospitalId = ParsingHelper.parseStringMethod(map['hospitalId']);
     scannerData = ParsingHelper.parseMapMethod<dynamic, dynamic, String, dynamic>(map['scannerData']);
     isActive = ParsingHelper.parseBoolMethod(map['isActive']);
+    isUndeletable = ParsingHelper.parseBoolMethod(map['isUndeletable']);
+    createdTime = ParsingHelper.parseTimestampMethod(map['createdTime']);
   }
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toMap({bool toJson = false}) {
     return <String, dynamic>{
       "id": id,
       "name": name,
@@ -52,16 +63,19 @@ class AdminUserModel extends Equatable {
       "role": role,
       "description": description,
       "imageUrl": imageUrl,
+      "hospitalId": hospitalId,
       "scannerData": scannerData,
       "isActive": isActive,
+      "isUndeletable": isUndeletable,
+      "createdTime": toJson ? createdTime?.toDate().toIso8601String() : createdTime,
     };
   }
 
   @override
-  String toString() {
-    return toMap().toString();
+  String toString({bool toJson = false}) {
+    return toMap(toJson: toJson).toString();
   }
 
   @override
-  List<Object?> get props => [id, name, username, password, role, description, imageUrl, isActive];
+  List<Object?> get props => [id, name, username, password, role, description, imageUrl, hospitalId, isActive, isUndeletable, createdTime];
 }
